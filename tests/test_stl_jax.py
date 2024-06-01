b1848c8488b8af2392dd17236fa8ba9d1d1196ba
Joe Eappen <joe.eappn@gmail.com>
2024-05-31 22:23:54 -0400

    feature(stl): add loop tests and stlpy solver tests

--- tests/test_stl_jax.py
+++ tests/test_stl_jax.py
@@ -12,13 +12,23 @@ import ds.utils as ds_utils
 import examples.stl.differentiability as stl_diff_examples
 from ds.stl_jax import STL, RectReachPredicate
 
-
+from ds.stl import StlpySolver
 class TestJAXExamples(unittest.TestCase):
 
     def setUp(self):
         os.environ["DIFF_STL_BACKEND"] = "jax"  # set the backend to JAX for all child processes
         importlib.reload(stl_diff_examples)  # Reload the module to reset the backend
         importlib.reload(ds_utils)  # Reload the module to reset the backend
+
+        self.goal_1 = STL(RectReachPredicate(np.array([0, 0]), np.array([1, 1]), "goal_1"))
+        # goal_2 is a rectangle area centered in [2, 2] with width and height 1
+        self.goal_2 = STL(RectReachPredicate(np.array([2, 2]), np.array([1, 1]), "goal_2"))
+
+        # form is the formula goal_1 eventually in 0 to 5 and goal_2 eventually in 0 to 5
+        # and that holds always in 0 to 8
+        # In other words, the path will repeatedly visit goal_1 and goal_2 in 0 to 13
+        self.form = (self.goal_1.eventually(0, 5) & self.goal_2.eventually(0, 5)).always(0, 8)
+        self.loop_form = (self.goal_1.eventually(0, 4) & self.goal_2.eventually(0, 4)).always(0, 8)
 
     def test_run(self):
         # TODO: Study jit decorator and see optimizations
@@ -43,14 +53,6 @@ class TestJAXExamples(unittest.TestCase):
 
     def test_evaluations(self, num_tiles=3):
         """Run simple evaluations to test shapes and types"""
-        goal_1 = STL(RectReachPredicate(np.array([0, 0]), np.array([1, 1]), "goal_1"))
-        # goal_2 is a rectangle area centered in [2, 2] with width and height 1
-        goal_2 = STL(RectReachPredicate(np.array([2, 2]), np.array([1, 1]), "goal_2"))
-
-        # form is the formula goal_1 eventually in 0 to 5 and goal_2 eventually in 0 to 5
-        # and that holds always in 0 to 8
-        # In other words, the path will repeatedly visit goal_1 and goal_2 in 0 to 13
-        form = (goal_1.eventually(0, 5) & goal_2.eventually(0, 5)).always(0, 8)
         path = ds_utils.default_tensor(
             np.array(
                 [
@@ -74,10 +76,42 @@ class TestJAXExamples(unittest.TestCase):
             )
         )
 
-        loss = form.eval(jax.numpy.tile(path, (num_tiles, 1, 1)))  # Make a batch of size num_tiles
+        loss = self.form.eval(jax.numpy.tile(path, (num_tiles, 1, 1)))  # Make a batch of size num_tiles
         self.assertGreater(len(loss.shape), 0, f"Not returning correct shape")
         self.assertEqual(loss.shape[0], num_tiles, f"Not returning {num_tiles} values")
 
+    def test_loop(self):
+        # Test loop spec
+        num_tiles = 4
+        path = ds_utils.default_tensor(
+            np.array(
+                [
+                    [
+                        [0, 0],
+                        [0, 2],
+                        [2, 0],
+                        [2, 2],
+                    ] * 3
+                ]))
+        loss = self.loop_form.eval(jax.numpy.tile(path, (num_tiles, 1, 1)))  # Make a batch of size num_tiles
+        self.assertGreater(len(loss.shape), 0, f"Not returning correct shape")
+        self.assertEqual(loss.shape[0], num_tiles, f"Not returning {num_tiles} values")
+        self.assertGreater(loss[0], 0, f"Loss is not greater than 0")
+
+        unsat_path = path.at[0, -4].set([0, 2])  # Make the last point unsatisfiable
+        loss = self.loop_form.eval(jax.numpy.tile(unsat_path, (num_tiles, 1, 1)))
+        self.assertLess(loss[0], 0, f"Loss is not less than 0 for unsat path")
+
+    def test_stlpy_solver(self):
+        x_0 = np.array([0, 0])
+        solver = StlpySolver(space_dim=2)
+        total_time = 12 # For loop spec
+        stlpy_form = self.loop_form.get_stlpy_form()
+        path, info = solver.solve_stlpy_formula(stlpy_form, x0=x_0, total_time=total_time)
+
+        num_tiles = 4
+        loss = self.loop_form.eval(jax.numpy.tile(path, (num_tiles, 1, 1)))  # Make a batch of size num_tiles
+        self.assertGreater(loss[0], 0, f"STLPY solved path loss is not greater than 0")
 
 if __name__ == '__main__':
     unittest.main()
